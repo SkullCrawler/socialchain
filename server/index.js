5d02e7cd371f5c3b7988c7a45a5bf4d259f44ee3
SkullCrawler <69508787+SkullCrawler@users.noreply.github.com>
2023-04-09 00:25:22 +0500

    ready for solidity!!!!

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ import { users, posts } from "./data/index.js";
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -52,21 +51,26 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 3001;
+dotenv.config();
+const PORT = process.env.PORT || 6001;
 mongoose
   .connect(
-    'mongodb+srv://SkullCrawler:SkullCrawler%40310275@cluster0.qfnp3ru.mongodb.net/?retryWrites=true&w=majority'
-  //   process.env.MONGO_URL, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // }
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
   )
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () =>{
+      console.log(`Server Port: ${PORT}`)
 
-    /* ADD DATA ONE TIME ONLY!!! */
+      /* ADD DATA ONE TIME ONLY!!! */
     // User.insertMany(users);
     // Post.insertMany(posts);
+    });
+
+    
   })
   .catch((error) => console.log(`${error} did not connect`));
